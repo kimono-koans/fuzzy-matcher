c5c093598fbb6a3290c48d667e912e85f885c627
electricboogie <32370782+kimono-koans@users.noreply.github.com>
2024-02-27 14:19:04 -0600

    Bump version

--- src/skim.rs
+++ src/skim.rs
@@ -39,7 +39,7 @@ const PENALTY_MAX_LEADING: ScoreType = -18;
 const PENALTY_UNMATCHED: ScoreType = -2;
 
 #[deprecated(since = "0.3.5", note = "Please use SkimMatcherV2 instead")]
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub struct SkimMatcher {}
 
 /// The V1 matcher is based on ForrestTheWoods's post
