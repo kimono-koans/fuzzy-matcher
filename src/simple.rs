818fd7cf292f20281461fcbfe9f51b32956c1961
electricboogie <32370782+kimono-koans@users.noreply.github.com>
2025-02-05 22:36:45 -0600

    Revert "Remove negative bonus for length of choice"
    This reverts commit 9f689ca26b4df320eddbb304cef6213068640650.

--- src/simple.rs
+++ src/simple.rs
@@ -202,11 +202,14 @@ impl<'a> SimpleMatch<'a> {
 
         let follows_special_char_bonus = self.follows_special_char(matches) * 4_096;
 
+        let len_neg = self.choice_len * 8;
+
         (closeness_score
             + start_idx_bonus
             + first_letter_case_bonus
             + follows_special_char_bonus
             + word_boundary_bonus
+            - len_neg
             - 65_536) as i64
     }
 
