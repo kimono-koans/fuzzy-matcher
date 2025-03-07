bce387e5920ae49e15940712f372b95743be8adc
electricboogie <32370782+kimono-koans@users.noreply.github.com>
2025-03-07 13:12:49 -0600

    Handle negative length score via tiebreak in skim/two_percent

--- src/simple.rs
+++ src/simple.rs
@@ -193,14 +193,11 @@ impl<'a> SimpleMatch<'a> {
 
         let follows_special_char_bonus = self.follows_special_char(matches) * 4_096;
 
-        let len_neg = self.choice_len * 16;
-
         (closeness_score
             + start_idx_bonus
             + follows_special_char_bonus
             + word_boundary_bonus
             + first_letter_case_bonus
-            - len_neg
             - 131_072) as i64
     }
 
